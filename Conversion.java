b52fab5ccb005eec3de6544209b64b46710717df
KEERTHANA-1413045 <keerthanakrishnan03@gmail.com>
2017-07-06 12:09:21 +0530

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,15 @@
+package Players;
+
+import java.util.Scanner;
+
+public class Conversion {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		int str1=Integer.parseInt(str);
+		System.out.println(str1);
+		sc.close();
+	}
+
+}
