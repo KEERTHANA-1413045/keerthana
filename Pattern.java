1cc88cf38b9fc3eb2d9c74720dab5a3c97049615
KEERTHANA-1413045 <keerthanakrishnan03@gmail.com>
2017-07-19 12:12:43 +0530

    Add files via upload

--- Pattern.java
+++ Pattern.java
@@ -1,16 +1,20 @@
-package Beginners;
+package Hunters;
 
 import java.util.Scanner;
+
 public class Pattern {
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		for(int i=0;i<n;i++){
-			for(int j=0;j<n;j++){
+		for(int i=n;i>0;i--){
+			for(int j=i;j>0;j--){
+				System.out.print("*");
+			}
+			System.out.print(" ");
+			for(int j=i;j>0;j--){
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		sc.close();
 	}
 }
