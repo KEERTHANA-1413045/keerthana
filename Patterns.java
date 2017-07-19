83661fd1d92156c31c307871bbd45d7f40ba5d77
KEERTHANA-1413045 <keerthanakrishnan03@gmail.com>
2017-07-19 12:16:18 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,21 @@
+package Hunters;
+
+import java.util.Scanner;
+
+public class Patterns {
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=n;i>0;i--){
+			for(int j=i;j>0;j--){
+				System.out.print("*");
+			}
+			System.out.print(" ");
+			for(int j=i;j>0;j--){
+				System.out.print("*");
+			}
+			System.out.println();
+			sc.close();
+		}
+	}
+}
