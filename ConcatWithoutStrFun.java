fd5cc1ba533ccfb59be4185215dc05894e9d2ef0
KEERTHANA-1413045 <keerthanakrishnan03@gmail.com>
2017-07-20 11:29:18 +0530

    Add files via upload

---
+++ ConcatWithoutStrFun.java
@@ -0,0 +1,15 @@
+package Players;
+
+import java.util.Scanner;
+
+public class ConcatWithoutStrFun {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		String str1=sc.next();
+		System.out.println(str+str1);
+		sc.close();
+	}
+
+}
