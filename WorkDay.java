9958fbdcaa76cc36636f930775f748a168adde89
KEERTHANA-1413045 <keerthanakrishnan03@gmail.com>
2017-07-20 17:25:12 +0530

    Add files via upload

---
+++ WorkDay.java
@@ -0,0 +1,21 @@
+package Hunters;
+
+import java.util.Scanner;
+
+public class WorkDay {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//String[] str={"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
+		String str1=sc.next();
+		if(str1.equals("sunday")){
+			System.out.println("false");
+		}
+			else
+			{
+				System.out.println("true");
+			}
+		sc.close();
+	}
+
+}
